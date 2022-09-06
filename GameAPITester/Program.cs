2d0eb1fa5509084ccfeab90fad5b25681767afa6
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2022-09-06 10:52:22 +0200

    Add project files.

---
+++ GameAPITester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace GameAPITester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string uri = "http://localhost:4000/api/games/1234";
+
+                using (WebClient client = new WebClient())
+                {
+                    Console.WriteLine(client.DownloadString(uri));
+                }
+
+                if (Console.ReadKey().Key == ConsoleKey.A)
+                    break;
+            }
+        }
+    }
+}
---
+++ GameWebApiBlog/Controllers/GamesController.cs
@@ -0,0 +1,58 @@
+﻿using GameWebApiBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameWebApiBlog.Controllers
+{
+public class GamesController : ApiController
+{
+    private static IList<Game> list = new List<Game>()
+{
+    new Game(){Id = 1234,Name="Mortal Kombat"},
+    new Game(){Id = 1235,Name="Defender of Crown"},
+    new Game(){Id = 1236,Name="Shadow of The Beast"},
+    new Game(){Id = 1237,Name="Dune II"},
+    new Game(){Id = 1238,Name="The Settlers"}
+};
+
+    // GET api/Games
+    [AcceptVerbs("GET")]
+    public IEnumerable<Game> AllGames()
+    {
+        return list;
+    }
+
+    [AcceptVerbs("GET", "POST")]
+    public Game RetriveGame(int id)
+    {
+        return list.First(e => e.Id == id);
+    }
+
+    // POST api/Games
+    [HttpPost]
+    public void AddGame(Game Game)
+    {
+        int maxId = list.Max(e => e.Id);
+        Game.Id = maxId + 1;
+        list.Add(Game);
+    }
+    // PUT api/Games/12345
+    [HttpPut]
+    public void UpdateGame(int id, Game Game)
+    {
+        int index = list.ToList().FindIndex(e => e.Id == id);
+        list[index] = Game;
+    }
+    // DELETE api/Games/12345
+    [HttpDelete]
+    public void DestroyGame(int id)
+    {
+        Game Game = RetriveGame(id);
+        list.Remove(Game);
+    }
+}
+}
